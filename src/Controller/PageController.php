b0f31b17179f9ca1551e137ecaf40a0cf97c31cf
Nhoni <nhmbemba@gmail.com>
2024-02-02 11:37:17 +0100

    Résolution du conflit dans page controller

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -14,60 +14,55 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
-   #[Route('/', name: 'home', methods: ['GET'])]
-   public function index(
-       CategoryRepository $categoryRepository,
-       PostRepository $postRepository,
-   ): Response {
-       $posts = $postRepository->findAll();
-       $categories= $categoryRepository->findAll();
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository, PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
-       return $this->render('page/home.html.twig', [
-           'posts' => $posts,
-           'categories' => $categories,
-       ]);
-   }
-
-//     #[Route('/{category}', name: 'category', methods: ['GET'])]
-//     public function category(
-//         Request $request,
-//         CategoryRepository $categoryRepository,
-//         PostRepository $postRepository,
-//         ): Response {
-//             $categoryName = $request -> get('category');
-//             $category = $categoryRepository->findOneBy([
-//                 'name'=> $categoryName]);
-//          if (!$category) {
-//                     throw $this->createNotFoundException('La catÃ©gorie n\'existe pas.');
-//                 }
-      
-//                 $posts = $postRepository->findBy(['category' => $category]);
-      
-//             return $this->render('page/category.html.twig', [
-//                 'category'=> $category,
-//                 'posts' => $postRepository->findBy(['category'=>$category]),
-//             ]);
-// }
-#[Route("/register", name: "app_register")]
-public function register(Request $request, EntityManagerInterface $em): Response
-{
-    $form = $this->createForm(RegisterType::class);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $user = $form->getData();
-
-        $em->persist($user);
-        $em->flush();
-
-        $this->addFlash('success', 'Inscription réussie! Connectez-vous maintenant.');
-
-        return $this->redirectToRoute('/');
+        return $this->render('page/home.html.twig', [
+            'posts' => $posts,
+            'categories' => $categories,
+        ]);
     }
 
-    return $this->render('registration/register.html.twig', [
-        'form' => $form->createView(),
-    ]);
-}
+    #[Route('/{category}', name: 'category', methods: ['GET'])]
+    public function category(Request $request, CategoryRepository $categoryRepository, PostRepository $postRepository): Response
+    {
+        $categoryName = $request->get('category');
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
 
+        if (!$category) {
+            throw $this->createNotFoundException('La catégorie n\'existe pas.');
+        }
+
+        $posts = $postRepository->findBy(['category' => $category]);
+
+        return $this->render('page/category.html.twig', [
+            'category' => $category,
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route("/register", name: "app_register")]
+public function register(Request $request, EntityManagerInterface $em): Response
+{
+   $form = $this->createForm(RegisterType::class);
+   $form->handleRequest($request);
+
+   if ($form->isSubmitted() && $form->isValid()) {
+       $user = $form->getData();
+
+       $em->persist($user);
+       $em->flush();
+
+       $this->addFlash('success', 'Inscription rÃ©ussie! Connectez-vous maintenant.');
+
+       return $this->redirectToRoute('/');
+   }
+
+   return $this->render('registration/register.html.twig', [
+       'form' => $form->createView(),
+   ]);
+}
 }
